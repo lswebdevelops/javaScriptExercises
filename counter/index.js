d19efcd57359d2056533f169bab71267f4ee08fd
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-25 13:39:39 -0300

    added counter to webpage

---
+++ counter/index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.body;
+    const divContainer = document.createElement('div') 
+    divContainer.classList.add("div-container")
+    body.appendChild(divContainer)
+    
+    const paragraph = document.createElement("p")
+    paragraph.classList.add("para1")
+    divContainer.appendChild(paragraph)
+    paragraph.innerText = `Count: `
+    
+    const span =  document.createElement("span")
+    paragraph.appendChild(span)
+    span.classList = "span"
+    span.innerText=""
+    
+    const div = document.createElement("div")
+    divContainer.appendChild(div)
+    div.classList.add("div-buttons")
+    
+    const button2 = document.createElement("button")
+    button2.classList.add("button-minus-counter")
+    div.appendChild(button2)
+    button2.innerText = `- Count`
+
+    const button = document.createElement("button")
+    button.classList.add("button-plus-counter")
+    div.appendChild(button)
+    button.innerText = `+ Count`
+       
+    button.addEventListener("click", increaseCount)
+    button2.addEventListener("click", decreaseCount)
+})
+
+let count = 0;
+
+function increaseCount() {
+    count ++;
+    const span = document.querySelector('.span')
+    span.innerText = count
+}
+
+const decreaseCount= () => {
+    count --;
+    const span = document.querySelector('.span')
+    span.innerText = count
+}
