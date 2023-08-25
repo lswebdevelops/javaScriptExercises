d6d483f46a4c742fe52494b3d2d3ade813bebc38
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-25 09:47:32 -0300

    adding a button to change all colors and background colors of page

--- colorFlipper/index.js
+++ colorFlipper/index.js
@@ -1,74 +1,89 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const container = document.body;
+  const body = document.body;
+  const divContainer = document.createElement("div");
   const div = document.createElement("div");
-  div.classList.add("div-container");
-
-  const paragraph = document.createElement("p");
-  paragraph.innerText = "Not to be changed";
-  paragraph.classList.add("para");
-  container.appendChild(div);
-
+  const para1 = document.createElement("p");
+  const para2 = document.createElement("p");
   const button = document.createElement("button");
-  div.appendChild(paragraph);
-  div.appendChild(button);
-  button.innerText = "Change Color";
+
+  para1.innerText = "Paragraph One";
+  para2.innerText = "Paragraph Two";
+  button.innerText = "Change Colors";
+
+  divContainer.classList.add("div-container");
+  para1.classList.add("para1");
+  para2.classList.add("para2");
   button.classList.add("button-color");
 
-  const paragraph2 = document.createElement("p");
-  div.appendChild(paragraph2);
-  paragraph2.innerText = "Text to be changed ";
-  paragraph2.classList.add("para2");
+  body.appendChild(divContainer);
+  divContainer.appendChild(div);
+  divContainer.appendChild(para1);
+  divContainer.appendChild(para2);
+  divContainer.appendChild(button);
 
-  const button2 = document.createElement("button");
-  button2.classList.add("button-background-color");
-  div.appendChild(button2);
-  button2.innerText = "Change Background";
-
-  button.addEventListener("click", colorToggle);
-  button2.addEventListener("click", backgroundColorToggle);
+  button.addEventListener("click", toggleColors);
 });
 
-function createRandomNumber() {
-  // return numbers from 0 to 255
+// create random number from 0 to 255
+const createRandomNumber = () => {
   let randomNumber = Math.floor(Math.random() * 256);
-  
   return randomNumber;
-}
+};
+
+// create random color
 
 const createRandomColor = () => {
+  // rgb;
   let red = createRandomNumber();
   let green = createRandomNumber();
   let blue = createRandomNumber();
   let randomColor = `rgb(${red},${green},${blue})`
-  console.log(randomColor);
-  return randomColor 
+  return randomColor
 };
 
-let isColorToggle1 = false;
+let isColorToggle = false;
 let isColorToggle2 = false;
+let isColorToggle3 = false;
 let isBackgroundColorToggle = false;
+let isBackgroundColorToggle2 = false;
 
-const colorToggle = () => {
-  const paragraph2 = document.querySelector(".para2");
-  const paragraph = document.querySelector(".para")
-  
-  paragraph2.style.color = isColorToggle1
-    ? `${createRandomColor()}`
-    : `${createRandomColor()}`;
-  isColorToggle1 = !isColorToggle1;
 
-  paragraph.style.color = isColorToggle2
+
+const toggleColors = () => {
+  const para1 = document.querySelector(".para1") 
+  const para2 = document.querySelector(".para2") 
+  const button = document.querySelector(".button-color") 
+  const divContainer = document.querySelector(".div-container") 
+
+  para1.style.color = isColorToggle
+  ? `${createRandomColor()}`
+  : `${createRandomColor()}`
+  isColorToggle = !isColorToggle
+
+  para2.style.color = isColorToggle2
   ? `${createRandomColor()}`
   : `${createRandomColor()}`
   isColorToggle2 = !isColorToggle2
-};
 
-const backgroundColorToggle = () => {
-  const div = document.querySelector(".div-container");
-  createRandomColor()
+  button.style.color = isColorToggle3
+  ? `${createRandomColor()}`
+  : `${createRandomColor()}`
+  isColorToggle3 = !isColorToggle3
 
-  div.style.backgroundColor = isBackgroundColorToggle
-    ? `${createRandomColor()}`
-    : `${createRandomColor()}`;
-  isBackgroundColorToggle = !isBackgroundColorToggle;
-};
+  divContainer.style.backgroundColor = isBackgroundColorToggle
+  ? `${createRandomColor()}`
+  : `${createRandomColor()}`
+  isBackgroundColorToggle = !isBackgroundColorToggle
+
+
+
+  button.style.backgroundColor = isBackgroundColorToggle2
+  ? `${createRandomColor()}`
+  : `${createRandomColor()}`
+  isBackgroundColorToggle2 = !isBackgroundColorToggle2
+
+
+
+
+}
+
